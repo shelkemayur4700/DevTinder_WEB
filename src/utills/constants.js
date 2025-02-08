459e140f47a2efbfd4b21e85331a336522c5987d
Mayurs0 <mayurs@iconnectsolutions.com>
2025-02-08 13:35:37 +0530

    [REFACT]:BaseURL updated

--- src/utills/constants.js
+++ src/utills/constants.js
@@ -1,1 +1,6 @@
-export const BASE_URL = "/api";
+// PRODUCTION
+// export const BASE_URL = "/api";
+
+// LOCAL DEVELOPMENT
+export const BASE_URL =
+  location?.hostname === "localhost" ? "http://localhost:7777" : "/api";
